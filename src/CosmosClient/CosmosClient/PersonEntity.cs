f03b8d435867283bb6766d13fa1f9c37cdfbb8ac
Michael Wolfgang <mike_wolfy@yahoo.com>
2020-04-06 08:53:30 -0400

    Added person repository

---
+++ src/CosmosClient/CosmosClient/PersonEntity.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Azure.Documents;
+using Newtonsoft.Json;
+
+namespace CosmosClient
+{
+    public class PersonEntity: Resource
+    {
+        [JsonProperty("partitionkey")]
+        public string PartitionKey
+        {
+            get { return $"person:{Id}"; }
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ src/CosmosClient/CosmosClient/PersonRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Azure.Documents;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CosmosClient
+{
+    public class PersonRepository : CosmosClientBase<PersonEntity>, IPersonRepository
+    {
+        public PersonRepository(IDocumentClient documentClient, IConfiguration configuration): base(
+            documentClient, configuration["DatabaseName"], configuration["PersonCollectionName"])
+        { }
+
+        public async Task<PersonEntity> GetPersonAsync(string personId)
+        {
+            try
+            {
+                var result = await GetAsync(x => x.Id == personId, new PartitionKey(new PersonEntity
+                {
+                    Id = personId
+                }.PartitionKey));
+
+                return result.FirstOrDefault();
+            }
+            catch (DocumentClientException e)
+            {
+                if (e.StatusCode == HttpStatusCode.NotFound)
+                {
+                    return null;
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        protected override PartitionKey GetPartitionKey(PersonEntity entity)
+        {
+            return new PartitionKey(entity.PartitionKey);
+        }
+    }
+}

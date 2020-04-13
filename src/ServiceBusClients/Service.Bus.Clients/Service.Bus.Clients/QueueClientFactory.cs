511a2d5db0e848155c0205c104f7985d00bd1b7e
Michael Wolfgang <mike_wolfy@yahoo.com>
2020-04-13 08:55:12 -0400

    Added some classes for managing service bus

---
+++ src/ServiceBusClients/Service.Bus.Clients/Service.Bus.Clients/QueueClientFactory.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace Service.Bus.Clients
+{
+    public class QueueClientFactory
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ConcurrentDictionary<string, IQueueClient> _queueClients = new ConcurrentDictionary<string, IQueueClient>();
+        /// <summary>
+        /// Constructor for QueueClientFactory
+        /// </summary>
+        /// <param name="configuration">Collection of deployment settings</param>
+        public QueueClientFactory(
+            IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        /// <summary>
+        /// Gets a queue client, either by fetching a previously instanced queue client or creating a new one
+        /// </summary>
+        /// <param name="serviceBusConnectionStringName">The connection string name to access the Azure Service Bus connection string in the configuration</param>
+        /// <param name="entityPath">The direct path to the entity</param>
+        /// <param name="createNewQueueClient">Flag to create new Queue client always of check if one exists</param>
+        /// <return>IQueueClient</return>
+        public IQueueClient GetQueueClient(string serviceBusConnectionStringName, string entityPath, bool createNewQueueClient = false)
+        {
+            if (createNewQueueClient)
+            {
+                return InitializeQueueClient(_configuration[serviceBusConnectionStringName], entityPath);
+            }
+
+            var key = $"{serviceBusConnectionStringName}-{entityPath}";
+
+            return _queueClients.AddOrReplaceIfClosed(key, () => InitializeQueueClient(_configuration[serviceBusConnectionStringName], entityPath));
+        }
+
+        [ExcludeFromCodeCoverage]
+        internal virtual IQueueClient InitializeQueueClient(string serviceBusConnectionString, string entityPath)
+        {
+            return new QueueClient(serviceBusConnectionString, entityPath);
+        }
+    }
+}

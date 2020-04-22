44288170a03f195bbfc0f9250fb8979dab863fb3
Michael Wolfgang <mike_wolfy@yahoo.com>
2020-04-22 09:07:49 -0400

    Added keyvault example

---
+++ src/KeyVaultProviders/mtw.keyvault.example/Program.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace mtw.keyvault.example
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                })
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                if (!hostingContext.HostingEnvironment.IsDevelopment())
+                {
+                    var builtConfig = config.Build();
+
+                    var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                    var keyVaultClient = new KeyVaultClient(
+                        new KeyVaultClient.AuthenticationCallback(
+                    azureServiceTokenProvider.KeyVaultTokenCallback));
+
+                    config.AddAzureKeyVault(
+                        $"https://{builtConfig["KeyVaultName"]}.vault.azure.net/",
+                        keyVaultClient,
+                        new DefaultKeyVaultSecretManager());
+                }
+                else
+                {
+                    var builtConfig = config.Build();
+                    config.AddAzureKeyVault(
+                        builtConfig.GetSection("KeyVault")["SecretURI"],
+                        builtConfig.GetSection("KeyVault")["ClientId"],
+                        builtConfig.GetSection("KeyVault")["Secret"]);
+                }
+            });
+    }
+}

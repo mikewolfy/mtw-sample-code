360351e451a7b4b850ccbb1379671cf656725e66
Michael T Wolfgang <149036@carmax.com>
2022-10-11 22:32:39 -0400

    More .NET 6 tests

---
+++ src/Net6UnitTests/Tests/Lambdas.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class Lambdas
+    {
+        [Fact]
+        private void TestFunct()
+        {
+            var msg = "Welcome to Lambda Improvements";
+            Func<string> welcome = () => "Welcome to Lambda Improvements";
+            Assert.Equal(msg, welcome.Invoke());
+        }
+
+        [Fact]
+        private void TestFunct2()
+        {
+            var msg = "one";
+            Func<string, string> doubleIt = (s) => $"{s}{s}";
+            Assert.Equal($"{msg}{msg}", doubleIt.Invoke(msg));
+        }
+    }
+}

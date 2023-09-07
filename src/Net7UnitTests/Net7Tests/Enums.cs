b8087aa7f70e1a808f6ea9f24104a1ae47a6f3d1
Michael T Wolfgang <149036@carmax.com>
2023-09-07 16:00:59 -0400

    Add some unit tests

---
+++ src/Net7UnitTests/Net7Tests/Enums.cs
@@ -0,0 +1,30 @@
+﻿namespace Net7Tests
+{
+    public class EnumTests
+    {
+
+        private enum Colors
+        {
+            Red,
+            Green,
+            Orange,
+            Yellow,
+            Blue,
+            Purple
+        }
+
+
+        [Fact]
+        public void Convert()
+        {
+            var color = Colors.Red;
+
+            var colorString = color.ToString();
+
+            Console.WriteLine(color);
+
+            Assert.Equal("Red", colorString);
+        }
+
+    }
+}
--- src/Net7UnitTests/Net7Tests/UnitTest1.cs
+++ src/Net7UnitTests/Net7Tests/UnitTest1.cs
@@ -1,3 +1,5 @@
+using Xunit.Sdk;
+
 namespace Net7Tests
 {
     public class UnitTest1
@@ -6,6 +8,18 @@ namespace Net7Tests
         public void Test1()
         {
             Assert.True(true);
+        }
+
+        [Fact]
+        public void SuppressNullability()
+        {
+            TestClass? testClass = null;
+            TestClass nonNullable = testClass!;
+        }
+
+        private class TestClass
+        {
+            public string Value { get; set; } = string.Empty;
         }
     }
 }
